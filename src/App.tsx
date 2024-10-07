0fbd62d8d94b96ec4cfcf39d684bdd71bc235f3c
billelbayou <bbillel2004@gmail.com>
2024-10-07 23:19:20 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <>
+      <header className="flex justify-between items-center p-4 shadow-lg">
+        <h1 className="text-3xl">Logo</h1>
+        <ul className="flex items-center text-xl">
+          <li className="m-2">About</li>
+          <li className="m-2">Contact</li>
+          <li className="m-2">Services</li>
+          <li className="m-2">Billel</li>
+        </ul>
+      </header>
+      <main>
+        
+      </main>
+    </>
+  )
+}
